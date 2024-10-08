49ab86d23076fb4a6a5289e99df02cd93a8efd44
nmmbfn <jhlove030219@naver.com>
2024-10-08 17:59:39 +0900

    full adder

---
+++ MY_fadder/MY_fadder.srcs/sources_1/new/f_hadder.v
@@ -0,0 +1,48 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/10/07 10:19:59
+// Design Name: 
+// Module Name: f_hadder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+module f_hadder(
+    input A,
+    input B,
+    input Ci,
+    output Co,
+    output S
+    );
+    wire ha0_s,ha0_c;
+    wire ha1_s,ha1_c;
+    
+    assign S=ha1_s;
+    assign Co=ha0_c | ha1_c;
+    
+   my_hadder ha0 (
+    .A(A),
+    .B(B),
+    .S(ha0_s),
+    .C(ha0_c)
+    );
+   
+    my_hadder ha1(
+    .A(ha0_s),
+    .B(Ci),
+    .S(ha1_s),
+    .C(ha1_c)
+    );
+    
+endmodule
